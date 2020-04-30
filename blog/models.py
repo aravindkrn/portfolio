38c195f21059362019ef845177882dc9614ff5dc
Aravind K <aravindkrn@gmail.com>
2020-04-30 11:54:56 +0530

    Polishing done on project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        if len(self.body) > 100:
+            return self.body[:100] + "..."
+        else:
+            return self.body
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
